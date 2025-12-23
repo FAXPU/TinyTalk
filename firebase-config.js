522809d0592c3ed02e995c39a69bda19301f2f67
FAXPU <faresel1212@gmail.com>
2025-12-24 01:58:01 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Replace these with your Firebase project keys
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBZvogTLy4LFDUFYMdv29cvyW4pLXZbgnA",
+    authDomain: "tinytalk-f1aac.firebaseapp.com",
+    projectId: "tinytalk-f1aac",
+    storageBucket: "tinytalk-f1aac.firebasestorage.app",
+    messagingSenderId: "5632445616",
+    appId: "1:5632445616:web:c24621da16a61ac6da3f75"
 };
 
 // Initialize Firebase

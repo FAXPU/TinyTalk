77015fe50d06a343657b32fe033ed88776b45860
FAXPU <faresel1212@gmail.com>
2025-12-24 01:35:00 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,17 @@
+import { auth } from "../firebase-config.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, user => {
+    if (!user) {
+        window.location.href = "login.html";
+    }
+});
+
+logoutBtn.addEventListener("click", () => {
+    signOut(auth).then(() => {
+        window.location.href = "login.html";
+    });
+});
 
